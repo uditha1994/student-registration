193c88d9260afbf2728ad412fe540417918d8f07
Uditha Landekumbura <51218166+uditha1994@users.noreply.github.com>
2024-11-11 12:10:24 +0530

    add edit.blade file

---
+++ resources/views/students/edit.blade.php
@@ -0,0 +1,13 @@
+<h2>Edit Student</h2>
+
+<form action="{{ route('students.update', $student->id) }}" method="post">
+
+    @csrf
+    <label for="name">Name :</label>
+    <input type="text" name="name" value="{{ $student->name }}">
+    <label for="email">Email :</label>
+    <input type="email" name="email" value="{{ $student->email }}">
+    <label for="phone">Phone :</label>
+    <input type="text" name="phone" value="{{ $student->phone }}">
+    <button type="submit">Update</button>
+</form>
